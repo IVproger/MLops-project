a4766e8bc664f98d9e96835156c48090d1847ca5
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-20 22:36:52 +0300

    Feature pulling

--- src/data_transformations.py
+++ src/data_transformations.py
@@ -1,5 +1,36 @@
-def transformation1():
-    print("Transformation 1")
-    
-def transformation2():
-    print("Transformation 2")
+"""Dataframe type for typings"""
+
+from pandas import DataFrame
+
+required = [
+    "FlightDate",
+    "Cancelled",
+    "DepTime",
+    "DepDelay",
+    "ArrTime",
+    "AirTime",
+    "ActualElapsedTime",
+    "Distance",
+    "Operating_Airline",
+    "Tail_Number",
+    "OriginAirportID",
+    "DestAirportID",
+    "ArrDelay",
+]
+
+
+def pull_features(df: DataFrame):
+    """
+    Extract only the required features from the dataframe
+    """
+    # Check that the required columns are there
+    for c in required:
+        print(c)
+        if c not in df.columns:
+            raise ValueError(
+                f"Dataframe lacks one or more of the required columns: {c}"
+            )
+    pulled_df = df.copy()
+    remaining_cols = set(df.columns) - set(required)
+
+    return pulled_df.drop(list(remaining_cols), axis=1)

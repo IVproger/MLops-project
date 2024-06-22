6e2f49a751a232d0e986fd57be9b5d6a2e68e253
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-06-22 22:05:05 +0300

    feat: add data quality validation

--- src/data.py
+++ src/data.py
@@ -1,17 +1,20 @@
-'''
+"""
 Import the necessary libraries and modules for the data sampling script.
-'''
+"""
 import os
 import gdown
 import pandas as pd
 from omegaconf import DictConfig, OmegaConf
 import hydra
 
+from src.data_quality import load_context_and_sample_data, define_expectations
+
+
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
-def sample_data(cfg: DictConfig) -> None:
-    '''
+def sample_data(cfg: DictConfig):
+    """
     The function to sample the data from the given URL and save it to the sample path.
-    '''
+    """
     try:
         datastore_path = cfg.data.datastore_path
         sample_path = cfg.data.sample_path
@@ -36,9 +39,36 @@ def sample_data(cfg: DictConfig) -> None:
         print("Error in loading or sampling the data: ", e)
         return None
 
+
+@hydra.main(version_base=None, config_path="../configs", config_name="main")
+def validate_initial_data(cfg: DictConfig):
+    """
+    Validate the data using Great Expectations.
+    """
+    try:
+        context, da = load_context_and_sample_data("../services", cfg.data.sample_path)
+        batch_request = da.build_batch_request()
+        validator = define_expectations(context, batch_request)
+        validator.save_expectation_suite(discard_failed_expectations=False)
+        checkpoint = context.add_or_update_checkpoint(
+            name="sample_checkpoint",
+            validator=validator,
+        )
+        checkpoint_result = checkpoint.run()
+
+        if checkpoint_result.success:
+            print("Validation failed.")
+            print(checkpoint_result)
+        else:
+            print("Validation successful.")
+    except Exception as e:
+        print("Error in validating the data: ", e)
+
+
 if __name__ == "__main__":
     cfg = OmegaConf.load("../configs/main.yaml")
     sample = sample_data(cfg)
     # save the generated sample of data
     sample.to_csv(cfg.data.sample_path, index=False)
-    
+    # validate the initial data
+    validate_initial_data(cfg)

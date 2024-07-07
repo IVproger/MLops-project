0e4e14e27aeb2f782389dd00685fb4adc3c300dd
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-07 09:45:49 +0300

    Removed old functions

--- src/data_transformations.py
+++ src/data_transformations.py
@@ -8,7 +8,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import FunctionTransformer
 
 from hydra import compose, initialize
-from deprecation import deprecated
 
 
 def sin_transformer(period):
@@ -68,101 +67,3 @@ def fix_hhmm(df: DataFrame, col: str) -> tuple[DataFrame, str, str]:
 
     df.drop([col], axis=1, inplace=True)
     return (df, colHH, colMM)
-
-
-@deprecated
-def fix_dtypes(df: DataFrame) -> DataFrame:
-    """Fixes datatypes for numerical columns
-
-    Args:
-        df (DataFrame): Source dataframe
-
-    Returns:
-        DataFrame: Source dataframe with types fixed
-    """
-    for c in [
-        "DepTime",
-        "DepDelay",
-        "ArrTime",
-        "AirTime",
-        "ActualElapsedTime",
-        "Distance",
-        "OriginAirportID",
-        "DestAirportID",
-        "ArrDelay",
-    ]:
-        df[c] = df[c].astype("int64")
-    return df
-
-
-@deprecated
-def encode_op_airline(df: DataFrame) -> DataFrame:
-    """Encode `Operating_Airline` with onehot encoding
-
-    Args:
-        df (DataFrame): Source dataframe
-
-    Returns:
-        Source dataframe with `Operating_Airline` onehotencoded
-    """
-    # Check that the column exists
-    if "Operating_Airline" not in df.columns:
-        raise ValueError(
-            "Operating_Airline column is expected in the dataframe, but not found"
-        )
-
-    # Check datatype
-    if df["Operating_Airline"].dtype is str:
-        raise ValueError("Operating_Airline column's datatype is not str")
-
-    df = pd.get_dummies(df, columns=["Operating_Airline"])
-    return df
-
-
-def handle_missing_values(df: DataFrame) -> DataFrame:
-    """Handle missing values in the dataframe
-
-    Args:
-        df (DataFrame): Source dataframe
-
-    Returns:
-        Source dataframe with missing values handled
-    """
-    # Check for missing values
-    if df.isnull().sum().sum() > 0:
-        # Fill missing values with 0
-        df.fillna(0, inplace=True)
-
-    return df
-
-
-def handle_duplicates(df: DataFrame) -> DataFrame:
-    """Handle duplicates in the dataframe
-
-    Args:
-        df (DataFrame): Source dataframe
-
-    Returns:
-        Source dataframe with duplicates handled
-    """
-    # Check for duplicates
-    if df.duplicated().sum() > 0:
-        # Drop duplicates
-        df.drop_duplicates(inplace=True)
-
-    return df
-
-
-def scale(df: DataFrame) -> DataFrame:
-    """Scale the dataframe using sklearn's StandardScaler
-
-    Args:
-        df (DataFrame): Source dataframe
-
-    Returns:
-        DataFrame: Source dataframe scaled
-    """
-    scaler = StandardScaler()
-    df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
-
-    return df

d7d51bd3b3c2d27a4a1608b01205d6b347c78d88
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-12 17:22:19 +0300

    fix: Fixed datasource

--- src/data.py
+++ src/data.py
@@ -154,7 +154,7 @@ def validate_features(
     """
     cfg = OmegaConf.load("configs/data_sample.yaml")
     context = gx.get_context(project_root_dir=cfg.data.context_dir_path, mode="file")
-    ds: PandasDatasource = context.add_or_update_datasource(name="transformed_sample")
+    ds: PandasDatasource = context.sources.add_or_update_pandas(name="transformed_sample")  
     ds.add_dataframe_asset(name="X", dataframe=X)
     ds.add_dataframe_asset(name="y", dataframe=y)
 

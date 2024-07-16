244b4042ae1cfc419aae827e49f41907183cd9bd
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-16 12:54:52 +0300

    Updated run

--- src/main.py
+++ src/main.py
@@ -1,17 +1,45 @@
 import hydra
-from src.model import fetch_features
+from omegaconf import DictConfig, OmegaConf
+from src.model import fetch_features, log_metadata, train
 
 
-def run(args):
-    cfg = args
-
+def run(cfg: DictConfig):
     train_data_version = cfg.train_data_version
 
+    print("Fetching features...")
     X_train, y_train = fetch_features(
-        name="features_target", version=train_data_version
+        name="features_target",
+        version=train_data_version,
+        cfg=cfg,
     )
 
-    print(X_train, y_train)
+    print("Starting training...")
+    gs = train(
+        X_train,
+        y_train,
+        cfg,
+    )
+
+    # test_data_version = cfg.test_data_version
+
+    print("Fetching testing features...")
+    X_test, y_test = fetch_features(
+        name="features_target",
+        version=train_data_version,
+        cfg=cfg,
+        test=True,
+    )
+
+    print("Logging metadata...")
+    log_metadata(
+        cfg,
+        gs,
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+    )
+
 
 
 @hydra.main(config_path="../configs", config_name="main", version_base=None)  # type: ignore

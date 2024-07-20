a7e5e3b14f767d4972e6dddeb0bd9ff9c3b31610
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-07-20 17:35:53 +0300

    feat: validate every model in 'models' directory

--- src/validate.py
+++ src/validate.py
@@ -7,7 +7,7 @@ import giskard.testing
 import mlflow
 import pandas as pd
 from omegaconf import DictConfig
-from src.model import retrieve_model_with_alias
+from src.model import scan_models_dir
 from src.data import extract_data, preprocess_data
 from src.utils import init_hydra
 
@@ -15,16 +15,27 @@ from src.utils import init_hydra
 def main():
     cfg = init_hydra("main")
 
-    # Wrap dataset and model
-    print("Wrapping dataset and model...")
+    print("Iterating over models")
+    for model_name, model_alias, model in scan_models_dir(cfg):
+        print(f"Validating model {model_name} with alias {model_alias}")
+        validate_model(cfg, model_name, model_alias, model)
+    print("Finished validation")
+
+
+def validate_model(
+    cfg: DictConfig, model_name: str, model_alias: str, model: mlflow.pyfunc.PyFuncModel
+):
+    # Wrap dataset
+    print("Wrapping dataset...")
     df, giskard_dataset, dataset_version = wrap_dataset(cfg)
-    model, model_version = wrap_model(cfg)
     print(f"Dataset version: {dataset_version}")
-    print(f"Model version: {model_version}")
+
+    # Define the prediction function
+    def predict_func(X: pd.DataFrame):
+        return model.predict(X)
 
     # Create giskard model object
     print("Creating Giskard model object...")
-    predict_func = get_predict_func(cfg, model)
     giskard_model = giskard.Model(
         model=predict_func,
         model_type="classification",
@@ -32,28 +43,26 @@ def main():
             cfg, raw_df, require_target=False
         )[0],
         classification_labels=list(cfg.model.labels),
-        # The order MUST be identical to the prediction_function's output order
-        feature_names=cfg.required,  # By default, all columns of the passed dataframe
-        name=cfg.model.best_model_name,  # Optional: give it a name to identify it in metadata
-        # classification_threshold=0.5, # Optional: Default: 0.5
+        feature_names=cfg.required,  # Validate only the required features
+        name=model_name,
+        classification_threshold=0.5,
     )
 
     # Run model scanning and generate html report
     print("Running model scan...")
     scan_model(
-        cfg,
         giskard_model,
         giskard_dataset,
-        f"validation_results_{cfg.model.best_model_name}_{model_version}_{cfg.data.dataset_name}_{dataset_version}.html",
+        f"validation_results_{model_name}_{model_alias}_{cfg.data.dataset_name}_{dataset_version}.html",
     )
 
     # Run test suite
     print("Running model tests...")
     test_results = run_model_tests(
-        cfg,
+        cfg.model.f1_threshold,
         giskard_model,
         giskard_dataset,
-        f"test_suite_{cfg.model.best_model_name}_{model_version}_{cfg.data.dataset_name}_{dataset_version}",
+        f"test_suite_{model_name}_{model_alias}_{cfg.data.dataset_name}_{dataset_version}",
     )
     if test_results.passed:
         print("Passed model validation!")
@@ -69,50 +78,18 @@ def wrap_dataset(cfg: DictConfig) -> tuple[pd.DataFrame, giskard.Dataset, str]:
     df, version = extract_data(version=cfg.test_data_version, cfg=cfg)
 
     # Wrap data into Giskard Dataset object
+    print(cfg.data.cat_cols)
     giskard_dataset = giskard.Dataset(
         df=df,
         target=cfg.data.target_cols[0],
         name=cfg.data.dataset_name,
+        # cat_columns=cfg.data.cat_cols,
     )
 
     return df, giskard_dataset, version
 
 
-def wrap_model(cfg: DictConfig) -> tuple[mlflow.pyfunc.PyFuncModel, int]:
-    """
-    Fetch model from the MLFlow model registry.
-    """
-    model_name = cfg.model.best_model_name
-    model_alias = cfg.model.best_model_alias
-
-    # Load model
-    model: mlflow.pyfunc.PyFuncModel = retrieve_model_with_alias(
-        model_name, model_alias=model_alias
-    )
-
-    # Get its version
-    client = mlflow.MlflowClient()
-    mv = client.get_model_version_by_alias(name=model_name, alias=model_alias)
-    model_version = mv.version
-
-    return model, model_version
-
-
-def get_predict_func(cfg: DictConfig, model: mlflow.pyfunc.PyFuncModel):
-    """
-    Custom predict function that preprocesses data and then predicts using the model.
-    """
-
-    def predict_func(X: pd.DataFrame):
-        # X, _ = preprocess_data(cfg, raw_df, validate_target=False)
-        return model.predict(X)
-
-    return predict_func
-
-
-def scan_model(
-    cfg: DictConfig, model: giskard.Model, dataset: giskard.Dataset, report_name: str
-):
+def scan_model(model: giskard.Model, dataset: giskard.Dataset, report_name: str):
     """
     Scan model using Giskard and save the results in html file.
     """
@@ -124,7 +101,7 @@ def scan_model(
 
 
 def run_model_tests(
-    cfg: DictConfig, model: giskard.Model, dataset: giskard.Dataset, suite_name: str
+    f1_threshold: float, model: giskard.Model, dataset: giskard.Dataset, suite_name: str
 ):
     """
     Create test suite using Giskard and run the tests.
@@ -132,7 +109,7 @@ def run_model_tests(
     # Declare test suite
     test_suite = giskard.Suite(name=suite_name)
     test1 = giskard.testing.test_f1(
-        model=model, dataset=dataset, threshold=cfg.model.f1_threshold
+        model=model, dataset=dataset, threshold=f1_threshold
     )
 
     # Run the test suite

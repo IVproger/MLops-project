e9a5e71f6a0d0fd108aaaafe8c9816d626c80f02
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-07-24 22:45:53 +0300

    ci: mkdir

--- src/download_sample.py
+++ src/download_sample.py
@@ -1,5 +1,8 @@
+import os
+
 import gdown
 
+os.mkdir(".dvc/datastore/files/md5/5e")
 gdown.download(
     "https://drive.google.com/uc?id=1OrOPS0GDsFno3FzukmOqPzETFsDYbiiU",
     ".dvc/datastore/files/md5/5e/e07f6e03b526e9164699b19e5036d9",

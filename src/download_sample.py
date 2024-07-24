ede456d274d9901c3dfd7537ff29087815285d17
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-07-24 22:47:41 +0300

    ci: mkdir via pathlib

--- src/download_sample.py
+++ src/download_sample.py
@@ -1,8 +1,8 @@
-import os
+from pathlib import Path
 
 import gdown
 
-os.mkdir(".dvc/datastore/files/md5/5e")
+Path(".dvc/datastore/files/md5/5e").mkdir(parents=True, exist_ok=True)
 gdown.download(
     "https://drive.google.com/uc?id=1OrOPS0GDsFno3FzukmOqPzETFsDYbiiU",
     ".dvc/datastore/files/md5/5e/e07f6e03b526e9164699b19e5036d9",

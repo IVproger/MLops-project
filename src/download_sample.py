68d47e1f7957ad5c7d1b631b905d34fda4b7c2bd
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-07-24 22:44:09 +0300

    ci: download to .dvc dir

--- src/download_sample.py
+++ src/download_sample.py
@@ -2,7 +2,7 @@ import gdown
 
 gdown.download(
     "https://drive.google.com/uc?id=1OrOPS0GDsFno3FzukmOqPzETFsDYbiiU",
-    "data/samples/sample.csv",
+    ".dvc/datastore/files/md5/5e/e07f6e03b526e9164699b19e5036d9",
     quiet=False,
     use_cookies=False,
 )
